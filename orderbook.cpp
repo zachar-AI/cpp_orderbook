c8feb001f3a24b59b87ebf1a7de95fd6eed5180e
zachar-AI <zachskihill@gmail.com>
2026-01-17 19:31:01 -0500

    added GetORderInfos

--- orderbook.cpp
+++ orderbook.cpp
@@ -1,8 +1,10 @@
+#include <cstddef>
 #include <cstdint>
 #include <functional>
 #include <list>
 #include <map>
 #include <memory>
+#include <numeric>
 #include <unordered_map>
 #include <vector>
 #include <format>
@@ -309,6 +311,30 @@ public:
     const auto& [existingOrder, _] = orders_.at(order.GetOrderId());
     CancelOrder(order.GetOrderId());
     return AddOrder(order.ToOrderPointer(existingOrder->GetOrderType()));
+  }
+
+  std::size_t Size() const { return orders_.size(); }
+
+  OrderbookLevelInfos GetOrderInfos() const
+  {
+    LevelInfos bidInfos, askInfos;
+    bidInfos.reserve(orders_.size());
+    askInfos.reserve(orders_.size());
+
+    auto CreateLevelInfos = [](Price price, const OrderPointers& orders)
+    {
+      return LevelInfo{price, std::accumulate(orders.begin(), orders.end(), (Quantity)0, 
+          [](std::size_t runningSum, const OrderPointer& order)
+          { return runningSum + order->GetRemainingQuantity(); }) };
+    };
+
+    for (const auto& [price, orders] : bids_)
+      bidInfos.push_back(CreateLevelInfos(price, orders));
+
+    for (const auto& [price, orders] : asks_)
+      askInfos.push_back(CreateLevelInfos(price, orders));
+
+    return OrderbookLevelInfos{ bidInfos, askInfos };
   }
 
 };
